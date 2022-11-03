615aaf41a746bbeae655705010cc1083e439cf34
LucasAndria <lucasandria01@gmail.com>
2022-11-03 20:49:55 +0300

    implement the factory function to Tour, User, Review

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,41 +1,19 @@
 const Review = require('../models/reviewModel');
 const AppError = require('../utils/appError');
-const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
+// const catchAsync = require('../utils/catchAsync');
 
-exports.getAllReviews = catchAsync(async (req, res, next) => {
-  let filter = {};
-  if (req.params.tourId) filter = { tour: req.params.tourId };
-
-  const reviews = await Review.find(filter);
-
-  if (!reviews) {
-    return next(new AppError('No reviews found', 400));
-  }
-
-  res.status(200).json({
-    status: 'success',
-    results: reviews.length,
-    data: {
-      reviews
-    }
-  });
-});
-
-exports.createReview = catchAsync(async (req, res, next) => {
+exports.setTourUserIds = (req, res, next) => {
   // Allow nested routes
   // Si les champs ne sont pas définie dans le body alors recuperer dans les params (/:tourId)
   if (!req.body.tour) req.body.tour = req.params.tourId;
   if (!req.body.user) req.body.user = req.user.id;
 
-  const newReview = await Review.create(req.body);
+  next();
+};
 
-  res.status(201).json({
-    status: 'success',
-    data: {
-      reviews: newReview
-    }
-  });
-});
-
+exports.getAllReviews = factory.getAll(Review);
+exports.getReview = factory.getOne(Review);
+exports.createReview = factory.createOne(Review);
+exports.updateReview = factory.updateOne(Review);
 exports.deleteReview = factory.deleteOne(Review);
