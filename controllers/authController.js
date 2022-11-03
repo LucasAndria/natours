727c6dedb2471280e34053068e2ce0ef6ee28903
LucasAndria <lucasandria01@gmail.com>
2022-11-03 21:00:56 +0300

    get current user data

--- controllers/authController.js
+++ controllers/authController.js
@@ -79,7 +79,7 @@ exports.protect = catchAsync(async (req, res, next) => {
   ) {
     token = req.headers.authorization.split(' ')[1];
   }
-  console.log(token);
+  console.log('connected user token : ', token);
 
   if (!token) {
     return next(
