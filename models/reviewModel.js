514909cf24bbc1a6a7f32916e94af5888ad844cd
LucasAndria <lucasandria01@gmail.com>
2022-11-04 09:41:38 +0300

    add static methode

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Tour = require('./tourModel');
 
 const reviewSchema = new mongoose.Schema(
   {
@@ -31,6 +32,56 @@ const reviewSchema = new mongoose.Schema(
     toOBJECT: { virtuals: true }
   }
 );
+
+// Static methode utiliser si on a besoin de aggregate
+reviewSchema.statics.calcAverageRatings = async function (tourId) {
+  const stats = await this.aggregate([
+    {
+      $match: { tour: tourId }
+    },
+    {
+      $group: {
+        _id: '$tour',
+        nRating: { $sum: 1 },
+        avgRating: { $avg: '$rating' }
+      }
+    }
+  ]);
+  // console.log(stats);
+
+  if (stats.length > 0) {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: stats[0].nRating,
+      ratingsAverage: stats[0].avgRating
+    });
+  } else {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: 0,
+      ratingsAverage: 4.5
+    });
+  }
+};
+
+// Post est appeler quand le document a été créer
+reviewSchema.post('save', function () {
+  // this point to the current review
+  this.constructor.calcAverageRatings(this.tour);
+});
+
+// findByIdAndUpdate
+// findByIdAndDelete
+// Pour récupérer le doc review et la stocker dans un var du this
+reviewSchema.pre(/^findOneAnd/, async function (next) {
+  this.review = await this.findOne();
+  // console.log(this.review);
+  next();
+});
+
+// exécuter calcAverageRatings avec le this.review
+reviewSchema.post(/^findOneAnd/, async function (next) {
+  // await this.findOne(); does NOT work here, query has already excecuted
+  await this.review.constructor.calcAverageRatings(this.review.tour);
+});
 
 // Query middleware
 reviewSchema.pre(/^find/, function (next) {
