16f02c16cbf317c688a4bede9f7185bb167f9300
LucasAndria <lucasandria01@gmail.com>
2022-11-30 21:50:52 +0300

    [FIX] data for tours madagascar

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -28,33 +28,33 @@ exports.uploadTourImages = upload.fields([
 ]);
 
 exports.resizeTourImages = catchAsync(async (req, res, next) => {
-  if (!req.files.imageCover || !req.files.images) return next();
+  // if (!req.files.imageCover || !req.files.images) return next();
 
-  // 1) Cover Image
-  req.body.imageCover = `tour-${req.params.id}-${Date.now()}-cover.jpeg`;
+  // // 1) Cover Image
+  // req.body.imageCover = `tour-${req.params.id}-${Date.now()}-cover.jpeg`;
 
-  await sharp(req.files.imageCover[0].buffer)
-    .resize(2000, 1333)
-    .toFormat('jpeg')
-    .jpeg({ quality: 90 })
-    .toFile(`public/img/tours/${req.body.imageCover}`);
+  // await sharp(req.files.imageCover[0].buffer)
+  //   .resize(2000, 1333)
+  //   .toFormat('jpeg')
+  //   .jpeg({ quality: 90 })
+  //   .toFile(`public/img/tours/${req.body.imageCover}`);
 
-  // 2) Images
-  req.body.images = [];
+  // // 2) Images
+  // req.body.images = [];
 
-  await Promise.all(
-    req.files.images.foreach(async (file, index) => {
-      const filename = `tour-${req.params.id}-${Date.now()}-${index + 1}.jpeg`;
+  // await Promise.all(
+  //   req.files.images.foreach(async (file, index) => {
+  //     const filename = `tour-${req.params.id}-${Date.now()}-${index + 1}.jpeg`;
 
-      await sharp(file.buffer)
-        .resize(2000, 1333)
-        .toFormat('jpeg')
-        .jpeg({ quality: 90 })
-        .toFile(`public/img/tours/${filename}`);
+  //     await sharp(file.buffer)
+  //       .resize(2000, 1333)
+  //       .toFormat('jpeg')
+  //       .jpeg({ quality: 90 })
+  //       .toFile(`public/img/tours/${filename}`);
 
-      req.body.images.push(filename);
-    })
-  );
+  //     req.body.images.push(filename);
+  //   })
+  // );
 
   next();
 });
