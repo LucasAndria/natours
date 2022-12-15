0c8b3967ce8a7c1e529398418b3dc7daa8fbf944
LucasAndria <lucasandria01@gmail.com>
2022-12-15 16:42:30 +0300

    [ADD] test

--- server.js
+++ server.js
@@ -1,52 +1,10 @@
-const mongoose = require('mongoose');
-// // for developpment only
-// const dotenv = require('dotenv');
+const express = require('express');
 
-process.on('uncaughtException', (err) => {
-  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
-  console.log(err.name, err.message);
-  process.exit(1);
-});
+const app = express();
 
-// // for developpment only
-// dotenv.config({ path: './config.env' });
-
-const app = require('./app');
-
-// MongoDB local
-// const DB = process.env.DATABASE_LOCAL;
-
-// MongoDB en ligne
-const DB = process.env.DATABASE.replace(
-  '<PASSWORD>',
-  process.env.DATABASE_PASSWORD
-);
-
-mongoose
-  .connect(DB, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  })
-  .then(() => console.log(`DB connection successful!`));
-
-// 4) START SERVER
-const PORT = process.env.PORT || 8000;
-
-// If you want to define the domain
-// const server = app.listen(PORT, '127.0.0.1', () => {
-//   console.log(`App running on port ${PORT}`);
-// });
-
-const server = app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}`);
-});
-
-//listen to the unhandledRejection event
-process.on('unhandledRejection', (err) => {
-  console.log(err.name, err.message);
-  console.log('UNHANDLER REJECTION! 💥 Shutting down...');
-  server.close(() => {
-    process.exit(1);
+app.get('/', (req, res) => {
+  res.status(200).json({
+    status: 'ok',
+    message: 'test123'
   });
 });
