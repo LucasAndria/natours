1ba2b6234873c8c27c85f05cdbb6be859de3f8c7
LucasAndria <lucasandria01@gmail.com>
2022-12-16 21:00:38 +0300

    [FIX] start debugging because it does not work when deployed

--- server.js
+++ server.js
@@ -1,26 +1,26 @@
 const mongoose = require('mongoose');
-// // for developpment only
-// const dotenv = require('dotenv');
+// for developpment only
+const dotenv = require('dotenv');
 
-// process.on('uncaughtException', (err) => {
-//   console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
-//   console.log(err.name, err.message);
-//   process.exit(1);
-// });
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
 
-// // for developpment only
-// dotenv.config({ path: './config.env' });
+// for developpment only
+dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 
 // MongoDB local
-// const DB = process.env.DATABASE_LOCAL;
+const DB = process.env.DATABASE_LOCAL;
 
-// MongoDB en ligne
-const DB = process.env.DATABASE.replace(
-  '<PASSWORD>',
-  process.env.DATABASE_PASSWORD
-);
+// // MongoDB en ligne
+// const DB = process.env.DATABASE.replace(
+//   '<PASSWORD>',
+//   process.env.DATABASE_PASSWORD
+// );
 
 mongoose
   .connect(DB, {
@@ -42,11 +42,11 @@ const server = app.listen(PORT, () => {
   console.log(`App running on port ${PORT}`);
 });
 
-// //listen to the unhandledRejection event
-// process.on('unhandledRejection', (err) => {
-//   console.log(err.name, err.message);
-//   console.log('UNHANDLER REJECTION! 💥 Shutting down...');
-//   server.close(() => {
-//     process.exit(1);
-//   });
-// });
+//listen to the unhandledRejection event
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLER REJECTION! 💥 Shutting down...');
+  server.close(() => {
+    process.exit(1);
+  });
+});
