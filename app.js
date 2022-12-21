6913ec2d7f92fccd2123f72f71df2201e1b2c30f
LucasAndria <lucasandria01@gmail.com>
2022-12-21 18:05:55 +0300

    [FIX] app and server.js

--- app.js
+++ app.js
@@ -27,8 +27,8 @@ app.set('views', path.join(__dirname, 'views'));
 // Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Set security HTTP headers
-app.use(helmet());
+// // Set security HTTP headers
+// app.use(helmet());
 
 // // Development console logging
 // if (process.env.NODE_ENV === 'development') {
